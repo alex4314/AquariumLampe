17d2811a5396f43c39670c3bc1526877fb2735a7
Alex <alexander.gecks@gmail.com>
2018-11-26 21:32:18 +0100

    Create Controller.ino

---
+++ Controller/Controller.ino
@@ -0,0 +1,21 @@
+/*
+  Controller
+  Created 2018-11-26
+*/
+
+struct action {
+  byte strip;
+  byte channel; 
+  unsigned int starttime; 
+  unsigned int duration;
+  byte brightness;  
+};
+
+
+void setup() {
+
+}
+
+void loop()  {
+
+}
