b38d45e43635a0ec9f0ef7455c318d4e24a12314
Alex <alexander.gecks@gmail.com>
2018-11-27 21:24:42 +0100

    Update LEDDriver.ino

--- LEDDriver/LEDDriver.ino
+++ LEDDriver/LEDDriver.ino
@@ -1,84 +1,102 @@
-/*
-  Fading
+#include <Wire_slave.h>
+#include <QueueArray.h>
 
-  This example shows how to fade an LED using the pwmWrite() function.
+struct animation {
+  unsigned int startpoint;
+  unsigned int duration;
+  byte target;
+};
 
-  Created 1 Nov 2008
-  By David A. Mellis
-  Modified 17 June 2009
-  By Tom Igoe
-
-  Modified by LeafLabs for Maple
-
-  http://arduino.cc/en/Tutorial/Fading
-
-  For differences between Maple's pwmWrite() and Arduino's analogWrite():
-  http://leaflabs.com/docs/lang/api/analogwrite.html#arduino-compatibility
-*/
+animation channel[7];
 
 int ledPin = PA8; // Connect an LED to digital pin 9, or any other
-// PWM-capable pin
-
-//HardwareTimer timer(1);
+byte message[32];
+byte modus = 0;
 
 void setup() {
-  pwmWrite(ledPin, 0);
+  //Serial.begin(9600);
+
   pinMode(ledPin, PWM);  // setup the pin as PWM
   pwmWrite(ledPin, 0);
-  //PWM_OPEN_DRAIN
-  Serial.begin(9600);
+
   Timer1.setPrescaleFactor(16);
   Timer1.setOverflow(4096);
-  delay(1000);
-  Serial.println("Beginn...");
 
-  //pinMode(PA0, PWM);
-  myservo.attach(PA0);  // attaches the servo on pin 9 to the servo object
-  myservo.write(0);
-  delay(5000);
-  myservo.write(180);
-  delay(5000);
-  myservo.write(0);
-  delay(5000);
-  myservo.write(180);
-  delay(5000);
-  myservo.write(0);
-  delay(5000);
+  Wire.begin(4);                // join i2c bus with address #4
+  Wire.onReceive(receiveEvent);
+
+  flushMessages();
+  for (byte i = 0; i < 7; i++) 
+    channel[i] = { 0 , 0 , 0 };
+  
 }
 
 void loop()  {
-  //  // Fade in from min to max in increments of 1280 points:
-  //  for (int fadeValue = 0; fadeValue <= 35; fadeValue++) {
-  //    // Sets the value (range from 0 to 65535):
-  //    pwmWrite(ledPin, fadeValue);
-  //    // Wait for 30 milliseconds to see the dimming effect:
-  //delay(1000);
-  //  }
-  //
-  for (int fadeValue = 0 ; fadeValue < 4096; fadeValue += 1) {
-    // Sets the value (range from 0 to 1280):
-    pwmWrite(ledPin, fadeValue);
-    // Wait for 30 milliseconds to see the dimming effect:
+  handleMessage();
 
-    Serial.print(" ...");
-    Serial.println(fadeValue);
-    delay(200);
+  switch (modus) {
+    case 10:
 
+      break;
+    case 20:
+
+      break;
+    case 30:
+
+      break;
+    case 40:
+
+      break;
   }
-  //
-  //pwmWrite(ledPin, 79);
-  //  delay(100);
-  //  pwmWrite(ledPin, 255);
-  //  delay(100);
 
+  for (int fadeValue = 0 ; fadeValue < 4096; fadeValue += 1) {
+    pwmWrite(ledPin, fadeValue);
+  }
+  int pos = 0;
   for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    //    myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(5);                       // waits 15ms for the servo to reach the position
   }
   for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    //    myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(5);                       // waits 15ms for the servo to reach the position
   }
 
+}
+
+void receiveEvent(int howMany) {
+  byte i = 0;
+  while (Wire.available()) { // loop through all but the last
+    message[i] = Wire.read(); // receive byte as a character
+    i++;
+  }
+}
+
+void handleMessage() {
+  switch (message[0]) {
+    case 0:
+      break;
+
+    case 21:     // new animation received
+      channel[message[1]].startpoint = millis() + message[2];
+      channel[message[1]].duration = combineByteToInt(message[3], message[4]);
+      channel[message[1]].target = message[5];
+      
+      flushMessages();
+      break;
+
+    default:
+      modus = message[0];
+  }
+}
+
+int combineByteToInt(byte high, byte low) {
+  int result= high << 8;
+  result |= low;
+  return result;
+}
+
+void flushMessages() {
+  for (byte i = 0; i < 32; i++)
+    message[i] = 0;
 }
